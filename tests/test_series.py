25145b3a1fff3281e0e8b7c9abe7e16da0c6ed44
Brian Lemons <brian_lemons@hotmail.com>
2021-05-18 16:17:20 -0700

    setting up tests and I think I've completed the fibonaci code

---
+++ tests/test_series.py
@@ -0,0 +1,18 @@
+from math_series.series import fibonacci
+
+
+# def test_one():
+#   actual = fibonacci(1)
+#   expected = 1
+#   assert actual == expected
+
+# def test_two():
+#   actual = fibonacci(2)
+#   expected = 1
+#   assert actual == expected
+
+
+def test_nine():
+  actual = fibonacci(9)
+  expected = 34
+  assert actual == expected
